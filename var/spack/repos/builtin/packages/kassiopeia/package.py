13abb75bdc116df61b7fb4288de306ceff5ea642
Wouter Deconinck <wdconinc@gmail.com>
2020-06-23 18:41:39 -0500

    [kassiopeia] new versions

--- var/spack/repos/builtin/packages/kassiopeia/package.py
+++ var/spack/repos/builtin/packages/kassiopeia/package.py
@@ -16,6 +16,11 @@ class Kassiopeia(CMakePackage):
     maintainers = ['wdconinc']
 
     version('master',  branch='master')
+    version('3.7.4', sha256='c1514163a084530930be10dbe487fb1950ccbc9662a4a190bdecffbd84a71fd4')
+    version('3.7.3', sha256='a8753585b9fa0903e1f5f821c4ced3cddd72792ad7e6075a7e25318f81ad9eaa')
+    version('3.7.2', sha256='bdfdf8c26fa5ad19e8b9c6eb600dfbd3c8218cd695ce067f10633b63bd192f92')
+    version('3.7.1', sha256='b22ae2fe5c2271bdf6aaf65d9ecf57ff0d6a88d28ad26d176e1129f0e58faea4')
+    version('3.7.0', sha256='32a3e98c77d1b97fe9862cf1d8c6ba8e6c82fb9295a6a217c7ce77cbec751046')
     version('3.6.1', sha256='30193d5384ad81b8570fdcd1bb35b15cc365ab84712819ac0d989c6f5cf6f790')
     version('3.5.0', sha256='b704d77bd182b2806dc8323f642d3197ce21dba3d456430f594b19a7596bda22')
     version('3.4.0', sha256='4e2bca61011e670186d49048aea080a06c3c95dacf4b79e7549c36960b4557f4')
